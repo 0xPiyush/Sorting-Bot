75646166c1134ad9dc11fecf223fea1cd27f40c4
real-Piyush-Sharma <piyush.sharma.bsp@gmail.com>
2020-11-02 03:39:06 +0530

    Unregister Command Added

--- cogs/mh.py
+++ cogs/mh.py
@@ -34,14 +34,15 @@ class mh(commands.Cog):
                 title=f':x: {ctx.author.name} is already registered for the Minecraft Manhunt.'))
             return None
 
-        if ign[0] == '\\':
-            ign = ign[1:]
+        reg_ign = ign
+        if reg_ign[0] == '\\':
+            reg_ign = reg_ign[1:]
 
-        if not self.ignValid(ign):
+        if not self.ignValid(reg_ign):
             await ctx.send(embed=Embed(title=f':x: Could not register the IGN: "{ign}", no such Minecraft Account found.'))
             return None
 
-        row = [ctx.author.name, ctx.author.discriminator, ign]
+        row = [ctx.author.name, ctx.author.discriminator, reg_ign]
 
         self.sheet.append_row(row)
         await ctx.send(embed=Embed(
@@ -54,18 +55,32 @@ class mh(commands.Cog):
                 title=f':x: {ctx.author.name} is not registered for the Minecraft Manhunt. Cannot update IGN without registering.'))
             return None
 
-        if ign[0] == '\\':
-            ign = ign[1:]
+        reg_ign = ign
+        if reg_ign[0] == '\\':
+            reg_ign = reg_ign[1:]
 
-        if not self.ignValid(ign):
+        if not self.ignValid(reg_ign):
             await ctx.send(embed=Embed(title=f':x: Could not register the IGN: "{ign}", no such Minecraft Account found.'))
             return None
 
         row_num = self.sheet.col_values(2).index(ctx.author.discriminator) + 1
 
-        self.sheet.update_cell(row_num, 3, ign)
+        self.sheet.update_cell(row_num, 3, reg_ign)
         await ctx.send(embed=Embed(
-            title=f':rocket: {ctx.author.name} registered IGN updated to "{ign}"'))
+            title=f':rocket: {ctx.author.name}\'s registered IGN updated to "{ign}"'))
+
+    @mh.command(aliases=['ur', 'ureg'])
+    async def unregister(self, ctx: commands.Context):
+        if ctx.author.discriminator not in self.sheet.col_values(2):
+            await ctx.send(embed=Embed(
+                title=f':x: {ctx.author.name} is not registered for the Minecraft Manhunt. Cannot Unregister without registering.'))
+            return None
+
+        row_num = self.sheet.col_values(2).index(ctx.author.discriminator) + 1
+
+        self.sheet.delete_row(row_num)
+        await ctx.send(embed=Embed(
+            title=f':rocket: {ctx.author.name} Unregisterd from the Minecraft Manhunt.'))
 
     def ignValid(self, ign: str):
         try:
