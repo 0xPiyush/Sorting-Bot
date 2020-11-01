df97a0580b4da6d8205589bc54187d1ac3a9d617
real-Piyush-Sharma <piyush.sharma.bsp@gmail.com>
2020-11-02 03:22:54 +0530

    added update methods

--- cogs/mh.py
+++ cogs/mh.py
@@ -1,7 +1,10 @@
+from functools import update_wrapper
+from discord import player
 from discord.ext import commands
 from discord import Embed
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from mojang_api import Player
 
 
 class mh(commands.Cog):
@@ -24,9 +27,9 @@ class mh(commands.Cog):
         if ctx.invoked_subcommand is None:
             await ctx.send(embed=Embed(title=':x: Error, incomplete Command, plesase provide a subcommand'))
 
-    @mh.command(aliases=['reg'])
+    @mh.command(aliases=['reg', 'r'])
     async def register(self, ctx: commands.Context, ign: str):
-        if ctx.author.discriminator in self.sheet.col_values(2)[1:]:
+        if ctx.author.discriminator in self.sheet.col_values(2):
             await ctx.send(embed=Embed(
                 title=f':x: {ctx.author.name} is already registered for the Minecraft Manhunt.'))
             return None
@@ -34,11 +37,42 @@ class mh(commands.Cog):
         if ign[0] == '\\':
             ign = ign[1:]
 
+        if not self.ignValid(ign):
+            await ctx.send(embed=Embed(title=f':x: Could not register the IGN: "{ign}", no such Minecraft Account found.'))
+            return None
+
         row = [ctx.author.name, ctx.author.discriminator, ign]
 
         self.sheet.append_row(row)
         await ctx.send(embed=Embed(
-            title=f':rocket: {ctx.author.name} registered for the Minecraft Manhunt.'))
+            title=f':rocket: {ctx.author.name} registered for the Minecraft Manhunt with IGN: "{ign}"'))
+
+    @mh.command(aliases=['upd', 'u'])
+    async def update(self, ctx: commands.Context, ign: str):
+        if ctx.author.discriminator not in self.sheet.col_values(2):
+            await ctx.send(embed=Embed(
+                title=f':x: {ctx.author.name} is not registered for the Minecraft Manhunt. Cannot update IGN without registering.'))
+            return None
+
+        if ign[0] == '\\':
+            ign = ign[1:]
+
+        if not self.ignValid(ign):
+            await ctx.send(embed=Embed(title=f':x: Could not register the IGN: "{ign}", no such Minecraft Account found.'))
+            return None
+
+        row_num = self.sheet.col_values(2).index(ctx.author.discriminator) + 1
+
+        self.sheet.update_cell(row_num, 3, ign)
+        await ctx.send(embed=Embed(
+            title=f':rocket: {ctx.author.name} registered IGN updated to "{ign}"'))
+
+    def ignValid(self, ign: str):
+        try:
+            player = Player(ign)
+            return True
+        except Exception:
+            return False
 
 
 def setup(Bot):
