f005fcd702214e893576f9b7b1392b3ef4b4d862
real-Piyush-Sharma <piyush.sharma.bsp@gmail.com>
2020-11-04 12:00:17 +0530

    Debugging

--- SortingBot.py
+++ SortingBot.py
@@ -150,7 +150,7 @@ async def list(ctx: commands.Context):
 
 @bot.command()
 async def SurpriseMaBoi(ctx: commands.Context):
-    print(ctx.message.guild)
+    print(bot.guilds)
     # for member in ctx.guild.members:
     #     print(member)
     # if member.discriminator == '0338':
